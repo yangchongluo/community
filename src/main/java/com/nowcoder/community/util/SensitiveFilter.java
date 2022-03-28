035a5aabba8abcb0612c2db66c576461e77f5349
Southrose <yangchongluo@163.com>
2022-03-28 14:58:56 +0800

    前缀树过滤敏感词

---
+++ src/main/java/com/nowcoder/community/util/SensitiveFilter.java
@@ -0,0 +1,175 @@
+package com.nowcoder.community.util;
+
+import org.apache.commons.lang3.CharUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component // 由容器托管
+public class SensitiveFilter {
+
+    private static final Logger logger = LoggerFactory.getLogger(SensitiveFilter.class);
+
+    // 把违禁词替换这个
+    private static final String REPLACEMENT = "***";
+
+    // 根节点
+    private TrieNode rootNode = new TrieNode();
+
+    @PostConstruct  // 这个注解表示，这是一个初始化的方法
+    // 当容器实例化这个Bean之后，调用构造器之后，就会自动调用这个方法
+    public void init() {
+        try (
+                // 使用类加载器加载文件
+                InputStream is = this.getClass().getClassLoader().getResourceAsStream("sensitive-words.txt");
+                // 字节流转成字符流，再转成缓冲流
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        ) {
+            String keyword;
+            while ((keyword = reader.readLine()) != null) {
+                // 添加到前缀树
+                this.addKeyword(keyword);
+            }
+        } catch (IOException e) {
+            logger.error("加载敏感词文件失败: " + e.getMessage());
+        }
+    }
+
+    /**
+     * 讲一个敏感词添加到前缀树上
+     *
+     * @param keyword 敏感词字符串
+     */
+    private void addKeyword(String keyword) {
+        TrieNode tempNode = rootNode;
+        for (int i = 0; i < keyword.length(); i++) {
+            char c = keyword.charAt(i);
+            TrieNode subNode = tempNode.getSubNode(c);
+
+            if (subNode == null) {
+                // 初始化子节点
+                subNode = new TrieNode();
+                tempNode.addSubNode(c, subNode);
+            }
+
+            // 指向子节点,进入下一轮循环
+            tempNode = subNode;
+
+            // 设置结束标识
+            if (i == keyword.length() - 1) {
+                tempNode.setKeywordEnd(true);
+            }
+        }
+    }
+
+    /**
+     * 过滤敏感词
+     *
+     * @param text 待过滤的文本
+     * @return 过滤后的文本
+     */
+    public String filter(String text) {
+
+        if (StringUtils.isBlank(text)) {
+            return null;
+        }
+        // 指针1，指向树的指针
+        TrieNode tempNode = rootNode;
+        // 指针2
+        int begin = 0;
+        // 指针3
+        int position = 0;
+        // 结果
+        StringBuilder sb = new StringBuilder();
+
+        while (begin < text.length()) {
+            if (position < text.length()) {
+                Character c = text.charAt(position);
+
+                // 跳过符号
+                if (isSymbol(c)) {
+                    if (tempNode == rootNode) {
+                        begin++;
+                        sb.append(c);
+                    }
+                    position++;
+                    continue;
+                }
+
+                // 检查下级节点
+                tempNode = tempNode.getSubNode(c);
+                if (tempNode == null) {
+                    // 以begin开头的字符串不是敏感词
+                    sb.append(text.charAt(begin));
+                    // 进入下一个位置
+                    position = ++begin;
+                    // 重新指向根节点
+                    tempNode = rootNode;
+                }
+                // 发现敏感词
+                else if (tempNode.isKeywordEnd()) {
+                    sb.append(REPLACEMENT);
+                    begin = ++position;
+                    tempNode = rootNode;
+                }
+                // 检查下一个字符
+                else {
+                    position++;
+                }
+            }
+            // position遍历越界仍未匹配到敏感词
+            else {
+                sb.append(text.charAt(begin));
+                position = ++begin;
+                tempNode = rootNode;
+            }
+        }
+        return sb.toString();
+    }
+
+    // 判断是否为符号
+    private boolean isSymbol(Character c) {
+        // 0x2E80~0x9FFF 是东亚文字范围
+        // CharUtils.isAsciiAlphanumeric(c)：判断c是不是正常的字符，如果是返回true
+        return !CharUtils.isAsciiAlphanumeric(c) && (c < 0x2E80 || c > 0x9FFF);
+    }
+
+    // 前缀树
+    private class TrieNode {
+
+        // 关键词结束标识
+        private boolean isKeywordEnd = false;
+
+        // 子节点(key是下级字符,value是下级节点)
+        private Map<Character, TrieNode> subNodes = new HashMap<>();
+
+        public boolean isKeywordEnd() {
+            return isKeywordEnd;
+        }
+
+        public void setKeywordEnd(boolean keywordEnd) {
+            isKeywordEnd = keywordEnd;
+        }
+
+        // 添加子节点
+        public void addSubNode(Character c, TrieNode node) {
+            subNodes.put(c, node);
+        }
+
+        // 获取子节点
+        public TrieNode getSubNode(Character c) {
+            return subNodes.get(c);
+        }
+
+    }
+
+}
