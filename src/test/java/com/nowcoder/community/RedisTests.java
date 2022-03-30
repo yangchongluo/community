400d2bab7feb8b0340335806c21208425666916c
Southrose <yangchongluo@163.com>
2022-03-30 21:12:40 +0800

    spring整合redis

---
+++ src/test/java/com/nowcoder/community/RedisTests.java
@@ -0,0 +1,159 @@
+package com.nowcoder.community;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.BoundValueOperations;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.concurrent.TimeUnit;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class RedisTests {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    // 字符串
+    @Test
+    public void testStrings() {
+        String redisKey = "test:count";
+
+        redisTemplate.opsForValue().set(redisKey, 1);
+
+        System.out.println(redisTemplate.opsForValue().get(redisKey));
+        System.out.println(redisTemplate.opsForValue().increment(redisKey));
+        System.out.println(redisTemplate.opsForValue().decrement(redisKey));
+    }
+
+    // hash
+    @Test
+    public void testHashes() {
+        String redisKey = "test:user";
+
+        redisTemplate.opsForHash().put(redisKey, "id", 1);
+        redisTemplate.opsForHash().put(redisKey, "username", "zhangsan");
+
+        System.out.println(redisTemplate.opsForHash().get(redisKey, "id"));
+        System.out.println(redisTemplate.opsForHash().get(redisKey, "username"));
+    }
+
+    // 列表
+    @Test
+    public void testLists() {
+        String redisKey = "test:ids";
+
+        redisTemplate.opsForList().leftPush(redisKey, 101);
+        redisTemplate.opsForList().leftPush(redisKey, 102);
+        redisTemplate.opsForList().leftPush(redisKey, 103);
+
+        System.out.println(redisTemplate.opsForList().size(redisKey));
+        System.out.println(redisTemplate.opsForList().index(redisKey, 0));
+        System.out.println(redisTemplate.opsForList().range(redisKey, 0, 2));
+
+        System.out.println(redisTemplate.opsForList().leftPop(redisKey));
+        System.out.println(redisTemplate.opsForList().leftPop(redisKey));
+        System.out.println(redisTemplate.opsForList().leftPop(redisKey));
+
+        redisTemplate.opsForList().leftPush(redisKey, 101);
+        redisTemplate.opsForList().leftPush(redisKey, 102);
+        redisTemplate.opsForList().leftPush(redisKey, 103);
+
+        System.out.println(redisTemplate.opsForList().rightPop(redisKey));
+        System.out.println(redisTemplate.opsForList().rightPop(redisKey));
+        System.out.println(redisTemplate.opsForList().rightPop(redisKey));
+
+    }
+
+    // 集合
+    @Test
+    public void testSets() {
+        String redisKey = "test:teachers";
+
+        redisTemplate.opsForSet().add(redisKey, "刘备", "关羽", "张飞", "赵云", "诸葛亮");
+
+        System.out.println(redisTemplate.opsForSet().size(redisKey));
+        System.out.println(redisTemplate.opsForSet().pop(redisKey));
+        System.out.println(redisTemplate.opsForSet().members(redisKey)); // 统计数据
+    }
+
+    // 有序集合
+    @Test
+    public void testSortedSets() {
+        String redisKey = "test:students";
+
+        redisTemplate.opsForZSet().add(redisKey, "唐僧", 80); // 后面的80是分数
+        redisTemplate.opsForZSet().add(redisKey, "悟空", 90);
+        redisTemplate.opsForZSet().add(redisKey, "八戒", 50);
+        redisTemplate.opsForZSet().add(redisKey, "沙僧", 70);
+        redisTemplate.opsForZSet().add(redisKey, "白龙马", 60);
+
+        System.out.println(redisTemplate.opsForZSet().zCard(redisKey));
+        System.out.println(redisTemplate.opsForZSet().score(redisKey, "八戒"));
+        // 去掉reverse就是从小到大排名了
+        System.out.println(redisTemplate.opsForZSet().rank(redisKey, "八戒"));
+        System.out.println(redisTemplate.opsForZSet().range(redisKey, 0, 2));
+        System.out.println(redisTemplate.opsForZSet().reverseRank(redisKey, "八戒"));
+        System.out.println(redisTemplate.opsForZSet().reverseRange(redisKey, 0, 2));
+    }
+
+    @Test
+    public void testKeys() {
+        redisTemplate.delete("test:user");
+
+        // 判断某个key存不存在
+        System.out.println(redisTemplate.hasKey("test:user"));
+
+        // 设置过期时间
+        redisTemplate.expire("test:students", 10, TimeUnit.SECONDS);
+    }
+
+    // 批量发送命令,节约网络开销.
+    @Test
+    public void testBoundOperations() {
+
+        // 使用redisTemplate绑定redisKey
+        String redisKey = "test:count";
+        BoundValueOperations operations = redisTemplate.boundValueOps(redisKey);
+        operations.increment();
+        operations.increment();
+        operations.increment();
+        operations.increment();
+        operations.increment();
+        System.out.println(operations.get());
+    }
+
+    // 编程式事务
+    @Test
+    public void testTransaction() {
+        Object result = redisTemplate.execute(new SessionCallback() {
+            @Override
+            public Object execute(RedisOperations redisOperations) throws DataAccessException {
+                String redisKey = "text:tx";
+
+                // 在开启事务和提交事务之之间，把命令放到队列里面了，不会立即执行
+                // 启用事务
+                redisOperations.multi();
+                redisOperations.opsForSet().add(redisKey, "zhangsan");
+                redisOperations.opsForSet().add(redisKey, "lisi");
+                redisOperations.opsForSet().add(redisKey, "wangwu");
+
+                System.out.println(redisOperations.opsForSet().members(redisKey));
+
+                // 提交事务
+                return redisOperations.exec();
+            }
+        });
+        // 每一个1代表三个命令影响的行数
+        System.out.println(result); // [1, 1, 1, [zhangsan, lisi, wangwu]]
+    }
+
+}
