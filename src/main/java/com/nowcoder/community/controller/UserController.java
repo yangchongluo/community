72fc13b49821d77d490d286d01fcc9f1a3a6d7ab
Southrose <yangchongluo@163.com>
2022-03-31 19:40:42 +0800

    收到的点赞

--- src/main/java/com/nowcoder/community/controller/UserController.java
+++ src/main/java/com/nowcoder/community/controller/UserController.java
@@ -1,9 +1,8 @@
 package com.nowcoder.community.controller;
 
-//import com.nowcoder.community.annotation.LoginRequired;
-
 import com.nowcoder.community.annotation.LoginRequired;
 import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.LikeService;
 import com.nowcoder.community.service.UserService;
 import com.nowcoder.community.util.CommunityUtil;
 import com.nowcoder.community.util.HostHolder;
@@ -24,7 +23,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/user")
@@ -46,6 +44,9 @@ public class UserController {
 
     @Autowired
     private HostHolder hostHolder;
+
+    @Autowired
+    private LikeService likeService;
 
     @LoginRequired
     @RequestMapping(path = "/setting", method = RequestMethod.GET)
@@ -111,17 +112,21 @@ public class UserController {
         }
     }
 
-    // 修改密码
-    @RequestMapping(path = "/updatePassword", method = RequestMethod.POST)
-    public String updatePassword(String oldPassword, String newPassword, Model model) {
-        User user = hostHolder.getUser();
-        Map<String, Object> map = userService.updatePassword(user.getId(), oldPassword, newPassword);
-        if (map == null || map.isEmpty()) {
-            return "redirect:/logout";
-        } else {
-            model.addAttribute("oldPasswordMsg", map.get("oldPasswordMsg"));
-            model.addAttribute("newPasswordMsg", map.get("newPasswordMsg"));
-            return "/site/setting";
+    // 个人主页
+    @RequestMapping(path = "/profile/{userId}", method = RequestMethod.GET)
+    public String getProfilePage(@PathVariable("userId") int userId, Model model) {
+        User user = userService.findUserById(userId);
+        if (user == null) {
+            throw new RuntimeException("该用户不存在!");
         }
+
+        // 用户
+        model.addAttribute("user", user);
+        // 点赞数量
+        int likeCount = likeService.findUserLikeCount(userId);
+        model.addAttribute("likeCount", likeCount);
+
+        return "/site/profile";
     }
+
 }
--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -4,6 +4,7 @@ public class RedisKeyUtil {
 
     private static final String SPLIT = ":";
     private static final String PREFIX_ENTITY_LIKE = "like:entity"; // 前缀
+    private static final String PREFIX_USER_LIKE = "like:user";
 
     /**
      * 某个实体的赞
@@ -15,6 +16,16 @@ public class RedisKeyUtil {
      */
     public static String getEntityLikeKey(int entityType, int entityId) {
         return PREFIX_ENTITY_LIKE + SPLIT + entityType + SPLIT + entityId;
+    }
+
+    /**
+     * 某个用户的赞
+     * @param userId
+     * @return
+     * like:user:userId -> int
+     */
+    public static String getUserLikeKey(int userId) {
+        return PREFIX_USER_LIKE + SPLIT + userId;
     }
 
 
