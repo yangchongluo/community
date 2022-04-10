627380aafdc76e9f7af8af03f75aaff7fcf18a77
Southrose <yangchongluo@163.com>
2022-04-10 15:36:23 +0800

    hyperloglog and bitmap

--- src/test/java/com/nowcoder/community/RedisTests.java
+++ src/test/java/com/nowcoder/community/RedisTests.java
@@ -5,10 +5,9 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.DataAccessException;
-import org.springframework.data.redis.core.BoundValueOperations;
-import org.springframework.data.redis.core.RedisOperations;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.SessionCallback;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.connection.RedisStringCommands;
+import org.springframework.data.redis.core.*;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -22,7 +21,6 @@ public class RedisTests {
     @Autowired
     private RedisTemplate redisTemplate;
 
-    // 字符串
     @Test
     public void testStrings() {
         String redisKey = "test:count";
@@ -34,7 +32,6 @@ public class RedisTests {
         System.out.println(redisTemplate.opsForValue().decrement(redisKey));
     }
 
-    // hash
     @Test
     public void testHashes() {
         String redisKey = "test:user";
@@ -46,7 +43,6 @@ public class RedisTests {
         System.out.println(redisTemplate.opsForHash().get(redisKey, "username"));
     }
 
-    // 列表
     @Test
     public void testLists() {
         String redisKey = "test:ids";
@@ -62,18 +58,8 @@ public class RedisTests {
         System.out.println(redisTemplate.opsForList().leftPop(redisKey));
         System.out.println(redisTemplate.opsForList().leftPop(redisKey));
         System.out.println(redisTemplate.opsForList().leftPop(redisKey));
-
-        redisTemplate.opsForList().leftPush(redisKey, 101);
-        redisTemplate.opsForList().leftPush(redisKey, 102);
-        redisTemplate.opsForList().leftPush(redisKey, 103);
-
-        System.out.println(redisTemplate.opsForList().rightPop(redisKey));
-        System.out.println(redisTemplate.opsForList().rightPop(redisKey));
-        System.out.println(redisTemplate.opsForList().rightPop(redisKey));
-
     }
 
-    // 集合
     @Test
     public void testSets() {
         String redisKey = "test:teachers";
@@ -82,15 +68,14 @@ public class RedisTests {
 
         System.out.println(redisTemplate.opsForSet().size(redisKey));
         System.out.println(redisTemplate.opsForSet().pop(redisKey));
-        System.out.println(redisTemplate.opsForSet().members(redisKey)); // 统计数据
+        System.out.println(redisTemplate.opsForSet().members(redisKey));
     }
 
-    // 有序集合
     @Test
     public void testSortedSets() {
         String redisKey = "test:students";
 
-        redisTemplate.opsForZSet().add(redisKey, "唐僧", 80); // 后面的80是分数
+        redisTemplate.opsForZSet().add(redisKey, "唐僧", 80);
         redisTemplate.opsForZSet().add(redisKey, "悟空", 90);
         redisTemplate.opsForZSet().add(redisKey, "八戒", 50);
         redisTemplate.opsForZSet().add(redisKey, "沙僧", 70);
@@ -98,9 +83,6 @@ public class RedisTests {
 
         System.out.println(redisTemplate.opsForZSet().zCard(redisKey));
         System.out.println(redisTemplate.opsForZSet().score(redisKey, "八戒"));
-        // 去掉reverse就是从小到大排名了
-        System.out.println(redisTemplate.opsForZSet().rank(redisKey, "八戒"));
-        System.out.println(redisTemplate.opsForZSet().range(redisKey, 0, 2));
         System.out.println(redisTemplate.opsForZSet().reverseRank(redisKey, "八戒"));
         System.out.println(redisTemplate.opsForZSet().reverseRange(redisKey, 0, 2));
     }
@@ -109,18 +91,14 @@ public class RedisTests {
     public void testKeys() {
         redisTemplate.delete("test:user");
 
-        // 判断某个key存不存在
         System.out.println(redisTemplate.hasKey("test:user"));
 
-        // 设置过期时间
         redisTemplate.expire("test:students", 10, TimeUnit.SECONDS);
     }
 
-    // 批量发送命令,节约网络开销.
+    // 多次访问同一个key
     @Test
     public void testBoundOperations() {
-
-        // 使用redisTemplate绑定redisKey
         String redisKey = "test:count";
         BoundValueOperations operations = redisTemplate.boundValueOps(redisKey);
         operations.increment();
@@ -133,27 +111,134 @@ public class RedisTests {
 
     // 编程式事务
     @Test
-    public void testTransaction() {
-        Object result = redisTemplate.execute(new SessionCallback() {
+    public void testTransactional() {
+        Object obj = redisTemplate.execute(new SessionCallback() {
             @Override
-            public Object execute(RedisOperations redisOperations) throws DataAccessException {
-                String redisKey = "text:tx";
+            public Object execute(RedisOperations operations) throws DataAccessException {
+                String redisKey = "test:tx";
 
-                // 在开启事务和提交事务之之间，把命令放到队列里面了，不会立即执行
-                // 启用事务
-                redisOperations.multi();
-                redisOperations.opsForSet().add(redisKey, "zhangsan");
-                redisOperations.opsForSet().add(redisKey, "lisi");
-                redisOperations.opsForSet().add(redisKey, "wangwu");
+                operations.multi();
 
-                System.out.println(redisOperations.opsForSet().members(redisKey));
+                operations.opsForSet().add(redisKey, "zhangsan");
+                operations.opsForSet().add(redisKey, "lisi");
+                operations.opsForSet().add(redisKey, "wangwu");
 
-                // 提交事务
-                return redisOperations.exec();
+                System.out.println(operations.opsForSet().members(redisKey));
+
+                return operations.exec();
             }
         });
-        // 每一个1代表三个命令影响的行数
-        System.out.println(result); // [1, 1, 1, [zhangsan, lisi, wangwu]]
+        System.out.println(obj);
+    }
+
+    // hyperloglog
+    // 统计20万个重复数据的独立总数.
+    @Test
+    public void testHyperLogLog() {
+        String redisKey = "test:hll:01";
+
+        for (int i = 1; i <= 100000; i++) {
+            redisTemplate.opsForHyperLogLog().add(redisKey, i);
+        }
+
+        for (int i = 1; i <= 100000; i++) {
+            int r = (int) (Math.random() * 100000 + 1);
+            redisTemplate.opsForHyperLogLog().add(redisKey, r);
+        }
+
+        long size = redisTemplate.opsForHyperLogLog().size(redisKey);
+        System.out.println(size);
+    }
+
+    // 将3组数据合并, 再统计合并后的重复数据的独立总数.
+    @Test
+    public void testHyperLogLogUnion() {
+        String redisKey2 = "test:hll:02";
+        for (int i = 1; i <= 10000; i++) {
+            redisTemplate.opsForHyperLogLog().add(redisKey2, i);
+        }
+
+        String redisKey3 = "test:hll:03";
+        for (int i = 5001; i <= 15000; i++) {
+            redisTemplate.opsForHyperLogLog().add(redisKey3, i);
+        }
+
+        String redisKey4 = "test:hll:04";
+        for (int i = 10001; i <= 20000; i++) {
+            redisTemplate.opsForHyperLogLog().add(redisKey4, i);
+        }
+
+        String unionKey = "test:hll:union";
+        redisTemplate.opsForHyperLogLog().union(unionKey, redisKey2, redisKey3, redisKey4);
+
+        long size = redisTemplate.opsForHyperLogLog().size(unionKey);
+        System.out.println(size);
+    }
+
+    // bitmap
+    // 统计一组数据的布尔值
+    @Test
+    public void testBitMap() {
+        String redisKey = "test:bm:01";
+
+        // 记录
+        redisTemplate.opsForValue().setBit(redisKey, 1, true);
+        redisTemplate.opsForValue().setBit(redisKey, 4, true);
+        redisTemplate.opsForValue().setBit(redisKey, 7, true);
+
+        // 查询
+        System.out.println(redisTemplate.opsForValue().getBit(redisKey, 0));
+        System.out.println(redisTemplate.opsForValue().getBit(redisKey, 1));
+        System.out.println(redisTemplate.opsForValue().getBit(redisKey, 2));
+
+        // 统计
+        Object obj = redisTemplate.execute(new RedisCallback() {
+            @Override
+            public Object doInRedis(RedisConnection connection) throws DataAccessException {
+                return connection.bitCount(redisKey.getBytes());
+            }
+        });
+
+        System.out.println(obj);
+    }
+
+    // 统计3组数据的布尔值, 并对这3组数据做OR运算.
+    @Test
+    public void testBitMapOperation() {
+        String redisKey2 = "test:bm:02";
+        redisTemplate.opsForValue().setBit(redisKey2, 0, true);
+        redisTemplate.opsForValue().setBit(redisKey2, 1, true);
+        redisTemplate.opsForValue().setBit(redisKey2, 2, true);
+
+        String redisKey3 = "test:bm:03";
+        redisTemplate.opsForValue().setBit(redisKey3, 2, true);
+        redisTemplate.opsForValue().setBit(redisKey3, 3, true);
+        redisTemplate.opsForValue().setBit(redisKey3, 4, true);
+
+        String redisKey4 = "test:bm:04";
+        redisTemplate.opsForValue().setBit(redisKey4, 4, true);
+        redisTemplate.opsForValue().setBit(redisKey4, 5, true);
+        redisTemplate.opsForValue().setBit(redisKey4, 6, true);
+
+        String redisKey = "test:bm:or";
+        Object obj = redisTemplate.execute(new RedisCallback() {
+            @Override
+            public Object doInRedis(RedisConnection connection) throws DataAccessException {
+                connection.bitOp(RedisStringCommands.BitOperation.OR,
+                        redisKey.getBytes(), redisKey2.getBytes(), redisKey3.getBytes(), redisKey4.getBytes());
+                return connection.bitCount(redisKey.getBytes());
+            }
+        });
+
+        System.out.println(obj);
+
+        System.out.println(redisTemplate.opsForValue().getBit(redisKey, 0));
+        System.out.println(redisTemplate.opsForValue().getBit(redisKey, 1));
+        System.out.println(redisTemplate.opsForValue().getBit(redisKey, 2));
+        System.out.println(redisTemplate.opsForValue().getBit(redisKey, 3));
+        System.out.println(redisTemplate.opsForValue().getBit(redisKey, 4));
+        System.out.println(redisTemplate.opsForValue().getBit(redisKey, 5));
+        System.out.println(redisTemplate.opsForValue().getBit(redisKey, 6));
     }
 
 }
